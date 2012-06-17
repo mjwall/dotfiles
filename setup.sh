be36287e0533197984d64a6e2cd3dd6821646d68
Michael Wall <mjwall@gmail.com>
2012-06-17 12:28:44 -0400

    Update setup.sh
    Also redo the README a little.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-CODE_HOME=${HOME}/src/Personal/dotfiles
+script_dir() {
+        if [ -z "${SCRIPT_DIR}" ]; then
+                # even resolves symlinks, see
+                # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+                    local SOURCE="${BASH_SOURCE[0]}"
+        while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+                SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    fi
+            echo "${SCRIPT_DIR}"
+}
+
+CODE_HOME=$(script_dir)
+
+setup_file_link() {
+    local source="$1"
+    local destination="$2"
+    if [ ! -e "${destination}" ]; then
+        ln -s "${source}" "${destination}" &&  echo "Linked file ${source} to ${destination}"
+    else
+        echo "ERROR: File ${source} already exists"
+    fi
+}
+
+setup_dir_link() {
+    local source="$1"
+    local destination="$2"
+    if [ ! -d "${destination}" ]; then
+        ln -s "${source}" "${destination}" && echo "Linked directory ${source} to ${destination}"
+    else
+        echo "ERROR: Directory ${source} already exists"
+    fi
+}
 # setup links
-ln -s "${CODE_HOME}/bashrc" ~/.bashrc
-ln -s "${CODE_HOME}/bash_profile" ~/.bash_profile
-ln -s "${CODE_HOME}/gitconfig" ~/.gitconfig
-ln -s "${CODE_HOME}/screenrc" ~/.screenrc
-ln -s "${CODE_HOME}/dotvim" ~/.vim
-ln -s ~/.vimrc ~/.vim/.vimrc
-ln -s "${CODE_HOME}/dotemacs.d" ~/.emacs.d
-ln -s "${CODE_HOME}/bin" ~/bin
-ln -s "${CODE_HOME}/bash_completion.d" ~/.bash_completion.d
+setup_file_link "${CODE_HOME}/bashrc" "${HOME}/.bashrc"
+setup_file_link "${CODE_HOME}/bash_profile" "${HOME}/.bash_profile"
+setup_file_link "${CODE_HOME}/gitconfig" "${HOME}/.gitconfig"
+setup_file_link "${CODE_HOME}/screenrc" "${HOME}/.screenrc"
+setup_dir_link "${CODE_HOME}/dotvim" "${HOME}/.vim"
+setup_dir_link "${CODE_HOME}/dotemacs.d" "${HOME}/.emacs.d"
+setup_dir_link "${CODE_HOME}/bin" "${HOME}/bin"
+setup_dir_link "${CODE_HOME}/bash_completion.d" "${HOME}/.bash_completion.d"
+setup_file_link "${HOME}/.vim/.vimrc" "${HOME}/.vimrc"
+
+# to remove everything, run the following.  Not going to automate this, because it is too destructive
+# rm "${HOME}/.bashrc" "${HOME}/.bash_profile" "${HOME}/.gitconfig" "${HOME}/.screenrc" "${HOME}/.vim" "${HOME}/.emacs.d" "${HOME}/bin" "${HOME}/.bash_completion.d" "${HOME}/.vimrc"
