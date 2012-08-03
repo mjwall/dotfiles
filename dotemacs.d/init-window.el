cbd517a9bff08386f905121787fbee707f383b54
Michael Wall <mjwall@gmail.com>
2012-08-03 18:42:21 -0400

    Fix window movement in terminal
    Well, at least give me other shortcuts.

--- dotemacs.d/init-window.el
+++ dotemacs.d/init-window.el
@@ -16,6 +16,12 @@
 
 (winner-mode 1)
 
+; movement keys C-<left> etc don't seem to work in terminal
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+
 ;; bind some window resizing
 (global-set-key (kbd "C-s-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-s-<right>") 'enlarge-window-horizontally)
