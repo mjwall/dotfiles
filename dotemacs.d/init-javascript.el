719c28fec4d20cf6a715b1d640562d7024d3a960
Michael Wall <mjwall@gmail.com>
2012-10-23 19:33:43 -0400

    Add jsonlint function
    Also add a json-pretty-format and nxml-pretty-format functions that format the selected region

--- dotemacs.d/init-javascript.el
+++ dotemacs.d/init-javascript.el
@@ -1,3 +1,10 @@
 (add-hook 'js-mode-hook 'run-coding-hook)
 
+(defun json-pretty-format ()
+    (interactive)
+    (save-excursion
+        (shell-command-on-region (point-min) (point-max) "jsonlint -" (buffer-name) t)
+        (indent-region begin end)))
+
+
 (provide 'init-javascript)
--- dotemacs.d/init-xml.el
+++ dotemacs.d/init-xml.el
@@ -9,4 +9,13 @@
 
 (add-hook 'nxml-mode-hook 'run-coding-hook)
 
+(defun nxml-pretty-format ()
+  "Function to format the current selected region"
+    (interactive)
+    (save-excursion
+        (shell-command-on-region (point-min) (point-max) "xmllint --format -" (buffer-name) t)
+        (nxml-mode)
+        (indent-region begin end)))
+
+
 (provide 'init-xml)
