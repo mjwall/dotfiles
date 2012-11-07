113eb12a990b4a18357d8fcad5633cb507a50dc5
Michael Wall <mjwall@gmail.com>
2012-11-07 08:33:05 -0500

    Turn off auto-fill in latex

--- dotemacs.d/init-text-mode.el
+++ dotemacs.d/init-text-mode.el
@@ -3,4 +3,7 @@
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'text-mode-hook 'turn-on-flyspell)
 
+;; turn off auto fill for latex
+(add-hook 'latex-mode-hook 'turn-off-auto-fill)
+
 (provide 'init-text-mode)
