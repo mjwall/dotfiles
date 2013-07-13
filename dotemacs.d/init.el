278ff74e1091c7434d283891ad8764bc72850636
Michael Wall <mjwall@gmail.com>
2013-07-13 10:15:10 -0400

    Add fullscreen magit and some other features
    Why did I resist magit so long?

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -679,6 +679,86 @@ there's a region, all lines that region covers will be duplicated."
 ;; Git stuff
 (require-package 'magit)
 
+;; next section from https://github.com/cjohansen/.emacs.d/blob/master/setup-magit.el
+(require 'magit-svn)
+
+;; Subtler highlight
+(set-face-background 'magit-item-highlight "#121212")
+(set-face-foreground 'diff-context "#666666")
+(set-face-foreground 'diff-added "#00cc33")
+(set-face-foreground 'diff-removed "#ff0000")
+
+;; Load git configurations
+;; For instance, to run magit-svn-mode in a project, do:
+;;
+;;     git config --add magit.extension svn
+;;
+(add-hook 'magit-mode-hook 'magit-load-config-extensions)
+
+;; C-x C-k to kill file on line
+
+(defun magit-kill-file-on-line ()
+  "Show file on current magit line and prompt for deletion."
+  (interactive)
+  (magit-visit-item)
+  (delete-current-buffer-file)
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "C-x C-k") 'magit-kill-file-on-line)
+
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+;; full screen vc-annotate
+
+(defun vc-annotate-quit ()
+  "Restores the previous window configuration and kills the vc-annotate buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :vc-annotate-fullscreen))
+
+(eval-after-load "vc-annotate"
+  '(progn
+     (defadvice vc-annotate (around fullscreen activate)
+       (window-configuration-to-register :vc-annotate-fullscreen)
+       ad-do-it
+       (delete-other-windows))
+
+     (define-key vc-annotate-mode-map (kbd "q") 'vc-annotate-quit)))
+
+;; ignore whitespace
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+;; end stuff from https://github.com/cjohansen/.emacs.d/blob/master/setup-magit.el
+
 ;; When I used git from the terminal and GIT_EDITOR=et, I need to close
 ;; the commit message when I am done.  C-x # is the command, which runs
 ;; server-edit.  But I do that so often, I want an easier key combo
