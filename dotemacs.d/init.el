a06317c5d4e8074d6bd86f53eedccf4f6ff9663f
Michael Wall <mjwall@gmail.com>
2013-12-20 13:22:47 -0500

    Adding JDEE,
    Figured it was time I actually tried it.  Not so bad so far, need to
    connect autocomplete or figure out how to use what is there.

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -1123,6 +1123,11 @@ Entered on %U
 ;; ---------
 (add-hook 'java-mode-hook 'run-coding-hook)
 
+;; JDEE mode - time to try it
+;; --------------------------
+(add-to-list 'load-path "~/.emacs.d/jdee-2.4.1/lisp")
+(load "jde")
+
 ;; JavaScript mode
 ;; ---------------
 (add-hook 'js-mode-hook 'run-coding-hook)
