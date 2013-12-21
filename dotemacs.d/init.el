c126d69f47705280e8b9f530a4e0203129e6e626
Michael Wall <mjwall@gmail.com>
2013-12-21 12:33:38 -0500

    Let's try jde-maven.el as well
    From http://www.emacswiki.org/emacs/download/jde-maven.el

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -1127,6 +1127,7 @@ Entered on %U
 ;; --------------------------
 (add-to-list 'load-path "~/.emacs.d/jdee-2.4.1/lisp")
 (load "jde")
+(require 'jde-maven)
 
 ;; JavaScript mode
 ;; ---------------
