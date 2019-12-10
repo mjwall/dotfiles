1ab1fe218a736a30b1444ca4cf1526483efe1fdb
Michael Wall <mjwall@gmail.com>
2019-12-10 20:40:54 +0000

    Remove old bins

--- bin/push-down-nb-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in `find . -name pom.xml | grep -v '\./pom.xml' | sed 's/pom.xml//'`; do 
-  #echo cp nb-configuration.xml $f
- cp nb-configuration.xml $f
-done
--- bin/start-mongo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$MONGO_HOME/bin/mongod --auth --dbpath $HOME/opt/mongo-data/db
-#$MONGO_HOME/bin/mongod --dbpath $HOME/opt/mongo-data/db
