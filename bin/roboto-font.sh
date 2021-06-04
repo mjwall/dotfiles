de2208d0f8b856d2ed4399cc5d647f6b784cbbfd
Michael Wall <mjwall@gmail.com>
2021-06-04 14:44:27 +0000

    Add roboto and roboto-slab font scripts

---
+++ bin/roboto-font.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+fonts_dir="${HOME}/.local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+else
+    echo "Found fonts dir $fonts_dir"
+fi
+
+for type in Black BlackItalic Bold BoldItalic Italic Light LightItalic Medium MediumItalic Regular Thin ThinItalic; do
+  file_path="${HOME}/.local/share/fonts/Roboto-${type}.ttf"
+  file_url="https://github.com/googlefonts/roboto/blob/main/src/hinted/Roboto-${type}.ttf?raw=true"
+  if [ ! -e "${file_path}" ]; then
+    echo "wget -O $file_path $file_url"
+    wget -O "${file_path}" "${file_url}"
+  else
+    echo "Found existing file $file_path"
+  fi;
+done
+
+echo "fc-cache -f"
+fc-cache -f
